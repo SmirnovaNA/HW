3dd9289a8170449ab3bedc8965037fdd9bd24bfd
SmirnovaNA <natsm2011@mail.ru>
2024-09-07 14:47:39 +0700

    Add files via upload

---
+++ lesson 1.py
@@ -0,0 +1,10 @@
+#1st programme
+print((9**0.5)*5)
+#2nd programe
+print(9.99>9.98 and 1000!=1000.1)
+#3nd program
+print(2*2+2)
+print(2*(2+2))
+print((2*2+2) == (2*(2+2)))
+#4th programe
+print(int(float('123.456')*10/308))

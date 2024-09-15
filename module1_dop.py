fb6f9468454d2731b96502ce97fb1d26130727d7
SmirnovaNA <natsm2011@mail.ru>
2024-09-15 21:05:04 +0700

    module1_dop

---
+++ module1_dop.py
@@ -0,0 +1,5 @@
+grades = [[5, 3, 3, 5, 4], [2, 2, 2, 3], [4, 5, 5, 2], [4, 4, 3], [5, 5, 5, 4, 5]]
+students = {'Johnny', 'Bilbo', 'Steve', 'Khendrik', 'Aaron'}
+st=sorted(students)
+gr = (sum(grades[0])/len(grades[0]), sum(grades[1])/len(grades[1]), sum(grades[2])/len(grades[2]),sum(grades[3])/len(grades[3]), sum(grades[4])/len(grades[4]))
+print({st[0]:gr[0],st[1]:gr[1],st[2]:gr[2],st[3]:gr[3],st[4]:gr[4]})

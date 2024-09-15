1484acf334b4e58bb46559e6c2ee40ab7925730e
SmirnovaNA <natsm2011@mail.ru>
2024-09-15 15:02:42 +0700

    module1_5

---
+++ module1_5.py
@@ -0,0 +1,8 @@
+immutable_var = ('Nalati', 54, 5.4, False, ['march', 'april', 'may'])
+print(immutable_var)
+print(type(immutable_var))
+#immutable_var[0] = 'Ivan'
+#print(immutable_var) кортэж = неизменяемый объект.
+mutable_list = ['march', 'april','may','june']
+mutable_list[3] = 6
+print(mutable_list)

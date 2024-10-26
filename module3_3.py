cdecc7670965aa0105a3d9779ffe50009930ec9a
SmirnovaNA <natsm2011@mail.ru>
2024-10-26 10:27:33 +0700

    Update module3_3.py

--- module3_3.py
+++ module3_3.py
@@ -2,35 +2,12 @@ def print_params(a = 1, b = 'строка', c = True):
     print(a, b, c)
 
 print_params()
-
-def print_params(*args):
-    print(args)
-
 print_params('Наташа', 1990,)
-print_params()
-
-def print_params(*args):
-    print(*args)
-
-print_params('Наташа', 1990,)
-
-def print_params(**kwargs):
-    print(*kwargs)
-
 print_params(b = 25)
 print_params(c = [1,2,3])
-
-def print_params(a, b, c):
-    print(a, b, c)
 values_list = ['Химия', 46, False]
 values_dict = {'a': 1990, 'b': 'Nata', 'c': False}
-
 print_params(*values_list)
 print_params(**values_dict)
-
-def print_params(a, b, c):
-    print(a, b, c)
-    print(c)
 values_list_2 = ['Химия', 46]
 print_params(*values_list_2, 42)
-

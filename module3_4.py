b4c9a670ac7473f1c60f1333fb2d4451757e381f
SmirnovaNA <natsm2011@mail.ru>
2024-10-28 21:41:30 +0700

    Update module3_4.py

--- module3_4.py
+++ module3_4.py
@@ -1,8 +1,8 @@
 def single_root_words(root_word, *other_words):
     same_words = []
     for i in range(len(other_words)):
-        if root_word.upper() in other_words[i].upper():
+        if root_word.upper() in other_words[i].upper() or other_words[i].upper() in root_word.upper():
             same_words.append(other_words[i])
-    print(same_words)
-single_root_words('rich', 'richiest', 'orichalcum', 'cheers', 'richies')
-single_root_words('Disablement', 'Able', 'Mable', 'Disable', 'Bagel')
+    return same_words
+print(single_root_words('rich', 'richiest', 'orichalcum', 'cheers', 'richies'))
+print(single_root_words('Disablement', 'Able', 'Mable', 'Disable', 'Bagel'))

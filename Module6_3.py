cb7e417bf7148bcc28b33137daac93a96f79879e
SmirnovaNA <natsm2011@mail.ru>
2024-11-28 22:11:54 +0700

    Update Module6_3.py

--- Module6_3.py
+++ Module6_3.py
@@ -3,17 +3,30 @@ class Animal:
     sound = None
     _DEGREE_OF_DANGER = 0
     _coords = [0, 0, 0]
-    _speed = ()
+
+    def __init__(self, _speed):
+        self._speed = _speed
+
     def move(self, dx, dy, dz):
-        self.dx = dx*_speed
-        self.dy = dy
-        self.dz = dz
+        x_new = dx*self._speed
+        y_new = dy*self._speed
+        z_new = dz*self._speed
+        if z_new < 0:
+            print("It's too deep, i can't dive :(")
+        else:
+            self._coords[0] = x_new
+            self._coords[1] = y_new
+            self._coords[2] = z_new
+
+        self.dx = dx*self._speed
+        self.dy = dy*self._speed
+        self.dz = dz*self._speed
 
     def get_cords(self):
-        return (f'X: {self.dx}, Y: {self.dy}, Z: {self.dz}')
+        print(f'X: {self.dx}, Y: {self.dy}, Z: {self.dz}')
 
     def attack(self):
-        if super._DEGREE_OF_DANGER < 5:
+        if self._DEGREE_OF_DANGER < 5:
             print("Sorry, i'm peaceful :)")
         else:
             print("Be careful, I'm attacking you 0_0")
@@ -27,12 +40,30 @@ class Bird(Animal):
 
 class AquaticAnimal(Animal):
     _DEGREE_OF_DANGER = 3
+
     def dive_in(self, dz):
-        self.dz = dz
-        return super._coords.appens(self.dz)
+        self._coords[2] = self._coords[2] - (abs(dz)*(self._speed/2))
 
 class PoisonousAnimal(Animal):
     _DEGREE_OF_DANGER = 8
 
-class Duckbill(Animal, Bird, AquaticAnimal):
+class Duckbill(Bird, AquaticAnimal, PoisonousAnimal):
     sound = "Click-click-click"
+
+    def speak(self):
+        print(self.sound)
+
+db = Duckbill(10)
+
+print(db.live)
+print(db.beak)
+
+db.speak()
+db.attack()
+
+db.move(1, 2, 3)
+db.get_cords()
+db.dive_in(6)
+db.get_cords()
+
+db.lay_eggs()
